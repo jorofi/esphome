e3e0f6f5cb4e5b2aead0df891e31aa15bdf40341
Georgi Filipov <georgi.filipov@uplink.bg>
2024-12-24 00:46:22 +0200

    Add climate IR Samsung component.

---
+++ esphome/components/climate_ir_samsung/climate.py
@@ -0,0 +1,22 @@
+import esphome.codegen as cg
+from esphome.components import climate_ir
+import esphome.config_validation as cv
+from esphome.const import CONF_ID
+
+CODEOWNERS = ["@jorofi"]
+
+AUTO_LOAD = ["climate_ir"]
+
+samsung_ns = cg.esphome_ns.namespace("samsung")
+SamsungClimate = samsung_ns.class_("SamsungClimate", climate_ir.ClimateIR)
+
+CONFIG_SCHEMA = climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend(
+    {
+        cv.GenerateID(): cv.declare_id(SamsungClimate)
+    }
+)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+
+    await climate_ir.register_climate_ir(var, config)
